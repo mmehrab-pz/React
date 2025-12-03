3dc2169c686361a76cf2723a6639a0d075d9790b
Mehrab <mmehrab.pk@gmail.com>
2025-12-03 15:14:07 +0330

    update

--- Tamrin/test-2/src/main.jsx
+++ Tamrin/test-2/src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-let name = prompt('emter your name')
-const x = 
-<>
-  <h1>{name} score : 50+50 = {50+50}</h1>
-  <p>hello world</p>
-</>;
+const data = ['mina' , 'sina' , 'ali' , 'tina']
+const myLiat = <ul>{data.map((val , i)=>{
+  return(
+    <li key={i}>{val}</li>
+  )
+})}</ul> 
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {x}
+    {myLiat}
   </StrictMode>,
 )
