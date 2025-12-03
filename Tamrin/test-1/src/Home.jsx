614d0098881eded74de1d091b1beb1fe22f77963
Mehrab <mmehrab.pk@gmail.com>
2025-12-03 13:46:29 +0330

    update

--- Tamrin/test-1/src/Home.jsx
+++ Tamrin/test-1/src/Home.jsx
@@ -2,15 +2,25 @@ import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
 import CourseBox from "./components/home/CourseBox";
 import CourseList from "./components/home/CourseList";
+import { useState } from "react";
+
 
 function Home() {
+  let [count , setCount] = useState(0)
+
+const counter = () =>{
+  
+  setCount(count+1)
+
+}
+
   return (
     <>
-      <CourseList>
-        <CourseBox title="course number one" details="hello world" time="100h" />
-      <CourseBox title="course number two" time="80h" />
-      <CourseBox title="course number three" details="hello next" time="150h" />
-      </CourseList>
+    <Header />  
+      <button onClick={counter}>click me</button>
+      <p>{count}</p>
+    <Footer />  
+
     </>
   );
 }
