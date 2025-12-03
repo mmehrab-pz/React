a19db314905249d38a0077a12d6d7c782e9e50f4
Mehrab <mmehrab.pk@gmail.com>
2025-12-03 14:31:33 +0330

    update

--- Tamrin/test-2/src/main.jsx
+++ Tamrin/test-2/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+let name = prompt('emter your name')
+const x = 
+<>
+  <h1>{name} score : 50+50 = {50+50}</h1>
+  <p>hello world</p>
+</>;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {x}
   </StrictMode>,
 )
