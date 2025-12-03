e07e9ef1996a4cc6d765056bce34aa429f79ed50
Mehrab <mmehrab.pk@gmail.com>
2025-12-03 15:30:04 +0330

    update

--- Tamrin/test-2/src/main.jsx
+++ Tamrin/test-2/src/main.jsx
@@ -1,16 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/master.css'
 
-const data = ['mina' , 'sina' , 'ali' , 'tina']
-const myLiat = <ul>{data.map((val , i)=>{
-  return(
-    <li key={i}>{val}</li>
-  )
-})}</ul> 
+const data = [
+  {
+    id:1,
+    name:'ali'
+  },
+  {
+    id:2,
+    name:'mina'
+  },
+  {
+    id:3,
+    name:'sina'
+  },
+  {
+    id:4,
+    name:'tina'
+  },
+
+]
+
+const myList = <ul>
+  {data.map((val , i)=>{
+    return(
+      <li className='list' key={i}>{val.id} : {val.name}</li>
+    )
+  })}
+</ul>
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {myLiat}
+    {myList}
   </StrictMode>,
 )
